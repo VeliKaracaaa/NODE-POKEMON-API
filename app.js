8214c0f78f388ceb599444860a366862c46ea247
veli <veli.karaca@epitech.eu>
2022-03-28 15:13:02 +0200

    add: retourner une liste de reponse et amélioration des points de terminaisons

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { success } = require('./helper');
 const pokemons = require('./mock-pokemon');
 
 const app = express()
@@ -9,12 +10,14 @@ app.get('/', (req, res) => res.send('Hello express 3!'))
 app.get('/api/pokemons/:id', (req, res) => {
     const id = parseInt(req.params.id)
     const pokemon = pokemons.find(pokemon => pokemon.id === id)
-    res.json(pokemon)
+    const message = "un pokemon a bien été trouvé."
+    res.json(success(message, pokemon))
 })
 
 //Nouveau point d'entrer
 app.get('/api/pokemons', (req, res) => {
-    res.send(`Il y a ${pokemons.length} pokémons dans le pokédex pour le moment.`)
+    const message = "La liste des pokemons a bien été récupérée."
+    res.json(success(message, pokemons))
 })
 
 app.listen(port, () => console.log(`Notre application Node est démarrée sur: http://localhost:${port}`))
