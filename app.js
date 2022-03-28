b936168c8d52943f1d994701693a030de8d8bcee
veli <veli.karaca@epitech.eu>
2022-03-28 02:50:29 +0200

    learn how to use methode req and enpoint get with params

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const pokemons = require('./mock-pokemon');
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello express 3!'))
+
+app.get('/api/pokemons/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const pokemon = pokemons.find(pokemon => pokemon.id === id)
+    res.send(`vous avez demandé le pokemon n°${pokemon.name}`)
+})
+
+//Nouveau point d'entrer
+app.get('/api/pokemons', (req, res) => {
+    res.send(`Il y a ${pokemons.length} pokémons dans le pokédex pour le moment.`)
+})
+
+app.listen(port, () => console.log(`Notre application Node est démarrée sur: http://localhost:${port}`))
