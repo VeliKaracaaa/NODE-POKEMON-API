45f7560a7ac1cf0d81c2a5a95837605f79d55450
veli <veli.karaca@epitech.eu>
2022-04-21 09:47:11 +0200

    push test

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("hello node !")
+const express = require('express')
+let pokemons = require('./mock-pokemon')
+const req = require('express/lib/request')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello, Express !'))
+
+app.get('/api/pokemons/:id', (req, res) => {
+    const id = req.params.id
+    const pokemon = pokemons.find(pokemon => pokemon.id === id)
+    res.send(`Hello vous avez demandé le pokemon n°${id}`)
+})
+
+app.listen(port, () => console.log(`Notre application Node est démarré sur : http://localhost:${port}`))
