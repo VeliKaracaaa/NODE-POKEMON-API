8eded47757a8bbb5635ba0ffb470df8ec6a244fc
veli <veli.karaca@epitech.eu>
2022-04-21 14:22:24 +0200

    relier nos données et les routes d'express

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const port = 3000
 app.get('/', (req, res) => res.send('Hello, Express !'))
 
 app.get('/api/pokemons/:id', (req, res) => {
-    const id = req.params.id
+    const id = parseInt(req.params.id)
     const pokemon = pokemons.find(pokemon => pokemon.id === id)
-    res.send(`Hello vous avez demandé le pokemon n°${id}`)
+    res.send(`Vous avez demandé le pokémon ${pokemon.name}`)
 })
 
 app.listen(port, () => console.log(`Notre application Node est démarré sur : http://localhost:${port}`))
