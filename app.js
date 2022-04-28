4716bbaf9f2af5d8f30c29d9f02298f9327f3b3a
veli <veli.karaca@epitech.eu>
2022-04-28 12:22:06 +0200

    combiner plusieur middlewere ensemble

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const morgan = require('morgan')
+const favicon = require('serve-favicon')
 let pokemons = require('./mock-pokemon')
 const { success } = require('./helper')
 
 const app = express()
 const port = 3000
 
-app.use(morgan('dev'))
+app
+    .use(favicon(__dirname + '/favicon.ico'))
+    .use(morgan('dev'))
 
 app.get('/', (req, res) => res.send('Hello, Express !'))
 
